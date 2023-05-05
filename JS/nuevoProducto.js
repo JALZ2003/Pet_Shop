964dd5493106ce24d35c56abc9814d934bb9f5bf
JALZ <jannus.largo@correounivalle.edu.co>
2023-05-05 01:34:04 -0500

    se desarrollo la logica para crear un nuevo producto

--- JS/nuevoProducto.js
+++ JS/nuevoProducto.js
@@ -8,21 +8,48 @@ function nuevoProducto() {
     } else if (document.getElementById('Juguete').checked) {
         tipoProducto = document.getElementById('Juguete').value
     }
-    let precioProducto = document.getElementById("precioProducto").value
-    let stockProducto = document.getElementById("stockProducto").value
+    let precioProducto = parseInt(document.getElementById("precioProducto").value);
+    let stockProducto = parseInt(document.getElementById("stockProducto").value);
 
     if (nombreProducto != 0 && imagenProducto != 0 && descripcionProducto != 0 && tipoProducto != 0 && precioProducto != 0 && stockProducto != 0 && nombreProducto != 0) {
         let producto = {
-            nombre: nombreProducto,
-            imagen: imagenProducto,
-            descripcion: descripcionProducto,
-            tipo: tipoProducto,
-            precio: precioProducto,
-            stock: stockProducto
+            "nombre": nombreProducto,
+            "imagen": imagenProducto,
+            "descripcion": descripcionProducto,
+            "tipo": tipoProducto,
+            "precio": precioProducto,
+            "stock": stockProducto
         }
 
-        fetch("https://pro-talento.up.railway.app/api/mindy/products", { method: 'POST', body: JSON.stringify(producto) })
-            .then(res => 'alerta para el éxito')
-            .catch(err => 'alerta para el catch')
+        fetch("https://pro-talento.up.railway.app/api/mindy/products", {
+            mode: 'cors',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(producto)
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Error en la solicitud POST');
+        }).then(producto => {
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡¡El producto se creo exitosamente!!',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }).catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${error}`
+            })
+        })
+        document.getElementById("nombreProducto").value = "";
+        document.getElementById("imagenProducto").value = "";
+        document.getElementById("descripcionProducto").value = "";
+        document.getElementById("precioProducto").value = 0;
+        document.getElementById("stockProducto").value = 0;
     }
 }
